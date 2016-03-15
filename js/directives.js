ebdbe7ee4251b42744825caf5e42798c032e76a9
Andrew Kharsun <andrewkharsun@Andrews-MacBook-Pro.local>
2016-03-15 18:05:44 +0200

    Finished Hide Content

--- js/directives.js
+++ js/directives.js
@@ -217,3 +217,28 @@ app.run(function($rootScope) {
         {address: "600 pennsylvania ave, washington DC",  "class": "my2"}
     ];
 });
+
+// Show/Hide Content
+app
+    .directive('sliderToggle', function() {
+        return {
+            restrict: 'AE',
+            link: function(scope, element, attrs) {
+                var target = element.next()[0];
+                element.parent().addClass('close');
+
+                attrs.expanded = false;
+
+                element.bind('click', function() {
+                    if(!attrs.expanded) {
+                        element.parent().removeClass('close').addClass('open');
+                        target.style.height = '16px';
+                    } else {
+                        element.parent().removeClass('open').addClass('close');
+                        target.style.height = 0;
+                    }
+                    attrs.expanded = !attrs.expanded;
+                });
+            }
+        }
+    });
